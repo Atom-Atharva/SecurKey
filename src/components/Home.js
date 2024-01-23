b0789809f4dc0a4ffe404bc1f2b8e2d43e9a7c06
Atharva Sugandhi <atharva.sugandhi@gmail.com>
2024-01-23 17:38:07 +0530

    Home Page Structuring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import Features from "./Features";
+import Footer from "./Footer";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Features />
+            <Footer />
+        </div>
+    );
 };
 
 export default Home;
